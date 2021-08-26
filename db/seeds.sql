9b9c5bd65032788a1b31168db7a231a6a55040a6
Will Berner <will.o.berner@gmail.com>
2021-08-26 14:03:10 -0400

    Created schema and seeds file to development.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,25 @@
+USE cms_db;
+
 INSERT INTO department (name)
 VALUES ('Production'),
        ('Marketing'),
        ('Accounting');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('Project Manager', 100,000.00, 1),
-       ('Engineer', 80,000.00, 1),
-       ('Manager', 85,000.00, 2),
-       ('Sales Representative', 60,000.00, 2),
-       ('Assistant Accountant', 65,000.00, 3),
-       ('Manager', 85,000.00, 3);
+VALUES ('Project Manager', 100000.00, 1),
+       ('Engineer', 80000.00, 1),
+       ('Manager', 85000.00, 2),
+       ('Sales Representative', 60000.00, 2),
+       ('Assistant Accountant', 65000.00, 3),
+       ('Account Manager', 85000.00, 3);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Bob', 'Robertson', 1, null),
+       ('John', 'Ragnarsson', 2, 1),
+       ('Will', 'Berner', 2, 1),
+       ('Sarah', 'Jones', 4, 2),
+       ('Emily', 'Johnson', 3, null),
+       ('Betty', 'Smith', 4, 2),
+       ('Ahmed', 'Islam', 6, null),
+       ('Tony', 'Hernandez', 5, 3);
