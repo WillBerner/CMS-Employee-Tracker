9bb76a9eb7c04a2b755c20fb9b4c17de599be8e4
Will Berner <will.o.berner@gmail.com>
2021-08-27 13:07:35 -0400

    Started on function for handling user input.

--- index.js
+++ index.js
@@ -22,9 +22,46 @@ async function init() {
             // Log any errors
             .catch(error => console.log(error));
 
+        determineUserInput(results.selection);
+
     // Continue to ask questions until user quits
     } while (results.selection !== "Quit");
 
+}
+
+// Determines which choice the user made
+function determineUserInput(selectionInput) {
+
+    switch (selectionInput) {
+        case "Quit":
+            return;
+        case "Add a department":
+            console.log("Add Dept!");
+            break;
+        case "Add a role":
+            console.log("Add role!");
+            break;
+        case "Add an employee":
+
+            break;
+        case "View all departments": 
+            console.log("View Dept!");
+            break;
+        case "View all roles":
+
+            break;
+        case "View all employees":
+            console.log("View employee!");
+            break;
+        case "Update an employee's role":
+            console.log("Update!");
+
+            break;
+        default:
+            return new Error("Error: Selection is not supported!"); 
+    }
+
+    return;
 }
 
 // Start the application up
