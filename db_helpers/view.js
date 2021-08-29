6f706d0329e7c126b34f19229e4f1824ab036888
Will Berner <will.o.berner@gmail.com>
2021-08-29 16:36:04 -0400

    Fixed left self join to display manager names rather than ids.

--- db_helpers/view.js
+++ db_helpers/view.js
@@ -11,7 +11,7 @@ export const viewDepartments = async (db) => {
 
 export const viewRoles = async (db) => {
 
-    let queryString = "SELECT role.id, role.title AS role_title, department.name AS department_name, role.salary FROM role JOIN department ON role.department_id = department.id";
+    let queryString = "SELECT role.id, role.title AS title, department.name AS department, role.salary FROM role JOIN department ON role.department_id = department.id";
 
     db.query(queryString, (err, rows) => {
         if (err) {
@@ -25,7 +25,7 @@ export const viewRoles = async (db) => {
 
 export const viewEmployees = async (db) => {
 
-    let queryString = "SELECT employee.id AS id, first_name, last_name, role_id, manager_id, title, salary, name AS department_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY id ASC";
+    let queryString = "SELECT e.id AS id, e.first_name, e.last_name, role.title AS title, name AS department, salary, CONCAT(m.first_name,' ', m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role ON e.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY id ASC";
 
     db.query(queryString, (err, rows) => {
         if (err) {
