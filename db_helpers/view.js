d1d3b7056f3ba354e5cfc9151bd06ca989f7972a
Will Berner <will.o.berner@gmail.com>
2021-08-29 15:48:31 -0400

    Added inquirer questions to add new data.

--- db_helpers/view.js
+++ db_helpers/view.js
@@ -25,7 +25,7 @@ export const viewRoles = async (db) => {
 
 export const viewEmployees = async (db) => {
 
-    let queryString = "SELECT employee.id AS id, first_name, last_name, role_id, manager_id, title, salary, name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY id ASC";
+    let queryString = "SELECT employee.id AS id, first_name, last_name, role_id, manager_id, title, salary, name AS department_name FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY id ASC";
 
     db.query(queryString, (err, rows) => {
         if (err) {
---
+++ questions/addQuestions.js
@@ -0,0 +1,48 @@
+export const addDepartmentQuestion = [
+    {
+        name: "department",
+        type: "input",
+        message: "What department would you like to add?"
+    }
+];
+
+export const addRoleQuestions = [
+    {
+        name: "deptName",
+        type: "input",
+        message: "To which department would you like to add this role?"
+    },
+    {
+        name: "roleName",
+        type: "input",
+        message: "What role would you like to add?"
+    },
+    {
+        name: "salary",
+        type: "input",
+        message: "What is the salary for this role?"
+    }
+];
+
+export const addEmployeesQuestions = [
+    {
+        name: "firstName",
+        type: "input",
+        message: "What is the employee's first name?"
+    },
+    {
+        name: "lastName",
+        type: "input",
+        message: "What is the employee's last name?"
+    },
+    {
+        name: "role",
+        type: "input",
+        message: "What will be the employee's role?"
+    },
+    {
+        name: "manager",
+        type: "input",
+        message: "Who is the manager of this employee, if any?"
+    }
+]
